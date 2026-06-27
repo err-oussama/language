8318178add342b1b54d883bbeb3d4bfa7ce2b035
Oussama Errami <oerrami@c3r8p2.1337.ma>
2026-06-27 17:15:35 +0100

    feat: arrow function

--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -1,16 +1,3 @@
 let log = (data: any) => {
   console.log(data);
 };
-
-// 1. String Literal Types
-// Instead of just 'string', it MUST be exactly one of these three words.
-type Theme = "light" | "dark" | "system";
-
-function getTheme(): Theme {
-  let thems: Theme[] = ["light"];
-
-  return thems[0];
-}
-let currentTheme: Theme = getTheme(); // OK
-
-log(currentTheme);
